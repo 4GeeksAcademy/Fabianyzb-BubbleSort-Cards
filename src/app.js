e2444f2e1d903e185ad768882ba6af8a766d4b54
Fabian Yanez <103224365+Fabianyzb@users.noreply.github.com>
2023-11-09 09:27:07 +0000

    nuevos cambios

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ window.onload = () => {
     .getElementById("generate-btn")
     .addEventListener("click", generateRandomCards);
 
-  // Agregar un evento al botón "Clasificar"
   document
     .getElementById("sort-btn")
     .addEventListener("click", sortAndShowChanges);
@@ -15,7 +14,6 @@ window.onload = () => {
 let generateRandomNumber = () => {
   let numbers = [
     "A",
-    "1",
     "2",
     "3",
     "4",
@@ -24,80 +22,96 @@ let generateRandomNumber = () => {
     "7",
     "8",
     "9",
-    "0",
+    "10",
     "J",
     "Q",
     "K"
   ];
-
   let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
 };
 
+// generar simbolo aleatorio
 let generateRandomSuit = () => {
-  // Lista de cartas
   let suit = ["diamond", "spade", "heart", "club"];
-  // Seleccionar carta aleatoria de la lista
+  // SELECCIONAR SIMBOLO
   let indexSuit = Math.floor(Math.random() * suit.length);
   return suit[indexSuit];
 };
 
-// Función para generar cartas aleatorias y mostrarlas en el contenedor
-let generateRandomCards = () => {
-  // Obtener el número de cartas que el usuario quiere generar
-  const numCards = parseInt(document.getElementById("num-cards").value, 10);
-  // Obtener el contenedor de cartas
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = ""; // Limpiar el contenedor
+// convertir valores de cartas
+function convertCardValue(value) {
+  switch (value) {
+    case "1":
+      return "A";
+    case "11":
+      return "J";
+    case "12":
+      return "Q";
+    case "13":
+      return "K";
+    default:
+      return value;
+  }
+}
 
-  // Generar y mostrar las cartas
+// func para generar cartas aleatorias y mostrarlas en el contenedor
+let generateRandomCards = () => {
+  const numCards = parseInt(document.getElementById("num-cards").value, 10);
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
   for (let i = 0; i < numCards; i++) {
     const card = document.createElement("div");
     card.classList.add("card");
     card.classList.add(generateRandomSuit());
-    card.innerHTML = generateRandomNumber();
+    const randomNumber = generateRandomNumber();
+    card.innerHTML = convertCardValue(randomNumber); // Aplicar la conversión
     cardContainer.appendChild(card);
   }
 };
 
-// Función para ordenar las cartas usando el BUBBLE SORT y mostrar los cambios
+// func para el BUBBLE SORT y mostrar los cambios
 function sortAndShowChanges() {
-  // Obtener el contenedor de cartas
   const cardsContainer = document.getElementById("card-container");
-  // Obtener todas las cartas
   const cards = Array.from(cardsContainer.querySelectorAll(".card"));
-  // Obtener el contenedor de cambios
   const cambiosDificilesContainer = document.getElementById(
     "cambios-dificiles-container"
   );
 
   cambiosDificilesContainer.innerHTML = ""; // Limpiar el registro
 
-  // Función de BUBBLE SORT!
   function bubbleSortWithChanges(cards) {
     const n = cards.length;
+    let step = 0; // Variable para llevar un registro del orden
 
     for (let i = 0; i < n - 1; i++) {
       for (let j = 0; j < n - i - 1; j++) {
         if (cards[j].innerHTML > cards[j + 1].innerHTML) {
-          // Realizar el intercambio de cartas
+          // Intercambiar las cartas
           const temp = cards[j].innerHTML;
           cards[j].innerHTML = cards[j + 1].innerHTML;
           cards[j + 1].innerHTML = temp;
 
-          // Agregar las cartas al registro horizontal después de cada cambio
-          cards.forEach(card => {
-            cambiosDificilesContainer.appendChild(card.cloneNode(true));
+          // Clonar cartas para mostrar etapas
+          const clonedCards = cards.map(card => card.cloneNode(true));
+
+          step++;
+          const stepLabel = document.createElement("span");
+          stepLabel.innerText = `PASO ${step}: `;
+          cambiosDificilesContainer.appendChild(stepLabel);
+          clonedCards.forEach(clonedCard => {
+            cambiosDificilesContainer.appendChild(clonedCard);
           });
+
+          // SEPARADOR
+          const separator = document.createElement("span");
+          separator.innerText = " | ";
+          cambiosDificilesContainer.appendChild(separator);
         }
       }
     }
   }
 
   bubbleSortWithChanges(cards);
-
-  // Agregar las cartas ordenadas al registro horizontal
-  cards.forEach(card => {
-    cambiosDificilesContainer.appendChild(card.cloneNode(true));
-  });
 }
